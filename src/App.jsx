5795ab5c17c34945353aaee81cd6fb34115d667f
Shweta Kanekar <kanekarshweta10@gmail.com>
2023-03-24 18:23:47 +0530

    Added logic to update color.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,15 @@ import { useEffect, useState } from 'react';
 function App() {
   const [quotes, setQuotes] = useState([{ text: '', author: '' }]);
   const [quoteId, setQuoteId] = useState(0);
+  const [quoteColor, setQuoteColor] = useState('tomato');
+
+  let bgColor = {
+    backgroundColor: quoteColor,
+  };
+
+  let color = {
+    color: quoteColor,
+  };
 
   useEffect(() => {
     getQuotes();
@@ -23,13 +32,36 @@ function App() {
   };
 
   const handleNewQuote = () => {
+    // select a random quote from quotes array
     let newId = Math.floor(Math.random() * quotes.length);
     setQuoteId(newId);
+
+    // select a random color from set of colors below
+    const colorSet = [
+      'green',
+      'navy',
+      'crimson',
+      'darkmagenta',
+      'lightseagreen',
+      'brown',
+      'coral',
+      'hotpink',
+      'royalblue',
+      'tomato',
+      'teal',
+      'deepskyblue',
+      'deeppink',
+      'greenyellow',
+      'peru',
+      'limegreen',
+    ];
+    let colorId = Math.floor(Math.random() * colorSet.length);
+    setQuoteColor(colorSet[colorId]);
   };
 
   return (
-    <div id="container">
-      <div id="quote-box">
+    <div id="container" style={bgColor}>
+      <div id="quote-box" style={color}>
         <div id="text">
           <span>
             <i className="fa fa-solid fa-quote-left"></i>
@@ -43,12 +75,13 @@ function App() {
           <a
             id="tweet-quote"
             href={`https://twitter.com/intent/tweet?text="${quotes[quoteId].text}"`}
+            style={bgColor}
           >
             <span>
               <i className="fa fa-brands fa-twitter"></i>
             </span>
           </a>
-          <button id="new-quote" onClick={handleNewQuote}>
+          <button id="new-quote" onClick={handleNewQuote} style={bgColor}>
             New quote
           </button>
         </div>

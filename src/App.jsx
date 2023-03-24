cd59c2c7ae686f4d2db9768a1e4c3b51a107d5ba
Shweta Kanekar <kanekarshweta10@gmail.com>
2023-03-24 16:24:51 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div id="container">
+      <div id="quote-box">
+        <div>Quote</div>
+        <p>Author</p>
+        <div>Footer</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

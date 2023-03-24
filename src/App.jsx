973906c0157804cd1693ddd23982cf390c65c18d
Shweta Kanekar <kanekarshweta10@gmail.com>
2023-03-24 16:37:17 +0530

    Integrated quotes API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+
+  useEffect(() => {
+    getQuotes();
+  }, []);
+
+  const getQuotes = () => {
+    axios
+      .get('https://type.fit/api/quotes')
+      .then((response) => {
+        setQuotes(response);
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log('Error: ', error);
+      });
+  };
+
   return (
     <div id="container">
       <div id="quote-box">
